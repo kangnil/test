16bc8a8675080078a709f95ce834d81e7fe3a569
kangnil <kangnil@andrew.cmu.edu>
2024-10-13 01:41:12 -0400

    fix

--- command.sh
+++ command.sh
@@ -1,21 +1,5 @@
-
-#CUDA_VISIBLE_DEVICES=1 python phc/run_hydra.py env.task=HumanoidAnyskillZ \
-#env=env_pulse_amp \
-#exp_name=pulse_anyskill_kickleftlegforwardrightlegretreats \
-#robot.real_weight_porpotion_boxes=False \
-#learning=pulse_z_task \
-#env.models=['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth'] \
-#env.motion_file=sample_data/amass_isaac_simple_run_upright_slim.pkl \
-#env.num_envs=1024 \
-#headless=True \
-#no_virtual_display=False
-
-CUDA_VISIBLE_DEVICES=0 python phc/run_hydra.py  \
-env.task=HumanoidSpeedZ \
-env=env_pulse_amp \
-exp_name=pulse_speed \
-robot.real_weight_porpotion_boxes=False \
-learning=pulse_z_task \
-env.models=['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth'] \
-env.motion_file=sample_data/amass_isaac_simple_run_upright_slim.pkl \
-env.num_envs=1024
+python phc/run_hydra.py env.task=HumanoidAnyskillZ env=env_pulse_amp exp_name=pulse_anyskill_dance env.text_yaml='phc/data/text_dance.yaml' robot.real_weight_porpotion_boxes=False learning=pulse_z_task env.models='['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth']' env.motion_file="sample_data/amass_isaac_simple_run_upright_slim.pkl" env.num_envs=1024
+python phc/run_hydra.py env.task=HumanoidAnyskillZ env=env_pulse_amp exp_name=pulse_anyskill_coil env.text_yaml='phc/data/text_coil.yaml' robot.real_weight_porpotion_boxes=False learning=pulse_z_task env.models='['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth']' env.motion_file="sample_data/amass_isaac_simple_run_upright_slim.pkl" env.num_envs=1024
+python phc/run_hydra.py env.task=HumanoidAnyskillZ env=env_pulse_amp exp_name=pulse_anyskill_kick env.text_yaml='phc/data/text_kick.yaml' robot.real_weight_porpotion_boxes=False learning=pulse_z_task env.models='['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth']' env.motion_file="sample_data/amass_isaac_simple_run_upright_slim.pkl" env.num_envs=1024
+python phc/run_hydra.py env.task=HumanoidAnyskillZ env=env_pulse_amp exp_name=pulse_anyskill_sit env.text_yaml='phc/data/text_sit.yaml' robot.real_weight_porpotion_boxes=False learning=pulse_z_task env.models='['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth']' env.motion_file="sample_data/amass_isaac_simple_run_upright_slim.pkl" env.num_envs=1024
+python phc/run_hydra.py env.task=HumanoidAnyskillZ env=env_pulse_amp exp_name=pulse_anyskill_coil env.text_yaml='phc/data/text_coil.yaml' robot.real_weight_porpotion_boxes=False learning=pulse_z_task env.models='['output/HumanoidIm/pulse_vae_iclr/Humanoid.pth']' env.motion_file="sample_data/amass_isaac_simple_run_upright_slim.pkl" env.num_envs=1024
